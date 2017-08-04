2d0cca70adca5396ab595866c4f21fb96c8f0cfc
U-DESKTOP-7BG9781\Muhammad <froma2zi@gmail.com>
2017-08-04 16:43:18 +0200

    Add all files

---
+++ README.md
@@ -0,0 +1,18 @@
+# Wonderful Quotes
+
+> this project will be add qoutes to remeber what are you doing
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+  <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
+</template>
+
+<script>
+  import QuoteGride from './Components/QuoteGrid.vue';
+export default {
+    data(){
+        return{
+            quotes:['just a Quote to See Something','Mahrous','Bakr','Hamdy','Mohtaseem','Hanafy'],
+            MaxQuotes:10
+        }
+    },
+    components:{
+        appQuoteGrid:QuoteGride
+    }
+}
+</script>
+
+<style>
+
+</style>
